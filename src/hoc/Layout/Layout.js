66562cafbbf8f636c61c07245c1a3c2c0bbf6e0f
CharithKularathna97 <charith@redtransact.com>
2021-09-28 18:40:52 +0530

    Intialization and Folder Structuring

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Layout extends Component{
+    render(){
+        return(
+            <>
+                <p>Welcome to MoleAR...!</p>
+            </>
+        )
+    }
+}
+
+export default Layout;
