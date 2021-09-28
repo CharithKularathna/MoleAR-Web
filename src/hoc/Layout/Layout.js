0f57d50e599ea32ab356a1b046b6aa2f61934252
Charith Kularathna <charithkularathna.17@cse.mrt.ac.lk>
2021-09-28 18:53:34 +0530

    Test to See Author

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -4,7 +4,7 @@ class Layout extends Component{
     render(){
         return(
             <>
-                <p>Welcome to MoleAR...!</p>
+                <p>Welcome to MoleAR...! </p>
             </>
         )
     }
